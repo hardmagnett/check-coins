5ae31766fc3ee3d6a4cc8d1fce570ce8425eb685
Taras Yemtsov <flamytwista@gmail.com>
2019-12-26 12:49:32 +0300

    Normalize.css

---
+++ src/components/CoinTable.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="coin-table">
+    <p>this is coin-table</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style scoped lang="scss">
+.coin-table {
+  outline: 1px solid darkred;
+}
+</style>
