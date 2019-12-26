49a1cff5e93b274b3cdd1a72b7f97f4726314484
Taras Yemtsov <flamytwista@gmail.com>
2019-12-26 13:57:35 +0300

    Containers pt1

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,33 @@
 <template>
   <div id="app">
-    <coin-table></coin-table>
+    <container
+      class="container--fluid container--top-just-for-design"
+    >
+    </container>
+    <container
+      class="container--no-padding-on-xs"
+    >
+      <coin-table></coin-table>
+    </container>
+
   </div>
 </template>
 <script>
   import CoinTable from '@/components/CoinTable.vue'
+  import Container from '@/components/Container.vue'
   export default {
-    components: {CoinTable}
+    components: {CoinTable, Container}
   }
 
 
 </script>
 <style lang="scss">
-
+.container--top-just-for-design {
+  background-color: $clrBg;
+  height: $gap*2;
+  @media (min-width: $bpSm) {
+    height: 100px;
+    margin-bottom: calc(-100px + #{$gap*2});
+  }
+}
 </style>
---
+++ src/components/Container.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="container">
+    <slot/>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style scoped lang="scss">
+.container {
+  padding-right: $gap;
+  padding-left: $gap;
+  margin-right: auto;
+  margin-left: auto;
+  /*outline: 1px solid darkred;*/
+  /*@media (min-width: $bpSm) {
+    padding-right: $gap;
+    padding-left: $gap;
+  }*/
+  @media (min-width: $bpSm2) {
+    max-width: $bpSm2;
+  }
+  @media (min-width: $bpMd) {
+    max-width: $bpMd;
+  }
+  @media (min-width: $bpMd) {
+    max-width: $bpMd;
+  }
+  @media (min-width: $bpLg) {
+    max-width: $bpLg;
+  }
+  @media (min-width: $bpXl) {
+    max-width: $bpXl;
+  }
+}
+.container--no-padding-on-xs {
+  padding-right: 0;
+  padding-left: 0;
+  @media (min-width: $bpSm) {
+    padding-right: $gap;
+    padding-left: $gap;
+  }
+}
+.container--fluid {
+  max-width: 100% !important;
+}
+</style>
