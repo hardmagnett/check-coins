b6a7df6d68faeeec2257b36cb5496bd590e00b48
Taras Yemtsov <flamytwista@gmail.com>
2019-12-29 15:02:54 +0300

    Websockets pt.1

--- src/main.js
+++ src/main.js
@@ -6,14 +6,47 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// вынести в отдельный файл
 import InfiniteLoading from 'vue-infinite-loading';
-
 Vue.use(InfiniteLoading, {});
+
+// вынести в отдельный файл, а может быть и в отдельные ф-и коннекта и дисконнекта
+import VueNativeSock from 'vue-native-websocket'
+Vue.use(VueNativeSock, 'wss://ws.coincap.io/prices?assets=ALL', {
+  store: store,
+  format: 'json',
+  reconnection: true,
+  connectManually: true,
+  passToStoreHandler: function (eventName, event) {
+    if (!eventName.startsWith('SOCKET_')) { return }
+    let msg = event.data ? JSON.parse(event.data) : {}
+
+    let myVuexMethodType = 'commit'             // по умолчанию вызывается мутация
+    let myVuexNamespace = ''                    // по умолчанию корневой модуль
+    let myVuexMethodName = eventName.toUpperCase()
+
+    if (
+      // если сообщение пришло из вебсокета о стоимости валюты
+      // возможно эта проверка будет не нужна пушо урл и так указан в опциях.
+      event.target.url.indexOf('ws.coincap.io/prices') !== -1
+      &&
+      eventName === 'SOCKET_onmessage'
+    ) {
+      myVuexMethodType = 'dispatch' // вызывает экшн
+      myVuexMethodName = 'SOCKET_ON_PRICE_CHANGE'
+    }
+    let myVuexMethodNameFull = myVuexNamespace + myVuexMethodName
+    this.store[myVuexMethodType](myVuexMethodNameFull, msg)
+  }
+})
 
 Vue.config.productionTip = false
 
-new Vue({
+let vm = new Vue({
   router,
   store,
   render: h => h(App)
 }).$mount('#app')
+
+// вынести в отдельный файл, а может быть и в отдельные ф-и коннекта и дисконнекта
+vm.$connect()
--- src/store/index.js
+++ src/store/index.js
@@ -7,8 +7,20 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {},
-  mutations: {},
-  actions: {},
+  mutations: {
+    // мутации по умолчанию, чтобы не ругалось в консоль что их нет.
+    SOCKET_ONOPEN () {},
+    SOCKET_ONCLOSE () {},
+    SOCKET_ONERROR () {},
+    SOCKET_ONMESSAGE () {},
+    SOCKET_RECONNECT() {},
+    SOCKET_RECONNECT_ERROR() {},
+  },
+  actions: {
+    SOCKET_ON_PRICE_CHANGE (state, message)  {
+      console.log(message); console.log('^...message in action:')
+    },
+  },
   modules: {},
   plugins: [VuexORM.install(database)]
 })
