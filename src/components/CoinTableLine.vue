db20cff6d0f2547dbf14e8be7969e5b1b542f24c
Taras Yemtsov <flamytwista@gmail.com>
2019-12-29 17:13:34 +0300

    animation pt1

--- src/components/CoinTableLine.vue
+++ src/components/CoinTableLine.vue
@@ -2,13 +2,19 @@
   <div class="coin-table-line">
     <coin-table-line-carcaas>
       <template v-slot:rank>
-        <p>{{asset.rank}}</p>
+        <p
+        >{{asset.rank}}</p>
       </template>
       <template v-slot:name>
         <coin-details :asset="asset"/>
       </template>
       <template v-slot:price>
-        <p>{{asset.priceUsdHumanReadable}}</p>
+        <p
+          :class="{'mod--flash-green': isPriceJustIncreased}"
+          @animationend="isPriceJustIncreased = false"
+        >
+          {{asset.priceUsdHumanReadable}}
+        </p>
       </template>
       <template v-slot:market-cap>
         <p>{{asset.marketCapUsdHumanReadable}}	</p>
@@ -39,6 +45,16 @@ export default {
       type: Asset,
       required: true
     }
+  },
+  data: function(){
+    return {
+      isPriceJustIncreased: false
+    }
+  },
+  watch: {
+    'asset.priceUsd': function(newVal, oldVal){
+      this.isPriceJustIncreased = true
+    }
   }
 }
 </script>
@@ -48,5 +64,14 @@ export default {
   .coin-details {
     width: 100%;
   }
+}
+.mod--flash-green {
+  /*animation: flash-green 1s ease-in-out 0.5s alternate;*/
+  animation: flashGreen 1s
+}
+@keyframes flashGreen {
+  from {background-color: white;}
+  50% {background-color: green;}
+  to {background-color: white;}
 }
 </style>
