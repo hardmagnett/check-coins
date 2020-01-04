ca4978537c915c4c726a2ce63963d1cb320eebec
Taras Yemtsov <flamytwista@gmail.com>
2020-01-05 01:16:32 +0300

    optimisations pt.2

--- src/orm/assets.js
+++ src/orm/assets.js
@@ -8,10 +8,39 @@ import _, {
   property as _property,
   wrap as _wrap
 } from 'lodash'
+let vuexModuleHelpers = {
+
+  canUpdateVolumeChangeCounter: function(state, coinId) {
+    const msForCoin = 5000
+    // return true
+    // console.log(state); console.log('^...state:')
+    let now = Date.now()
+    let result = false
+    if (!state.assetVolumeChangeCounerUpdages[coinId]) {
+      state.assetVolumeChangeCounerUpdages[coinId] = {
+        lastUpdateTS: now
+      }
+      result = true
+    } else if (
+      state.assetVolumeChangeCounerUpdages[coinId]
+      && state.assetVolumeChangeCounerUpdages[coinId].lastUpdateTS + msForCoin < now
+    ){
+      state.assetVolumeChangeCounerUpdages[coinId] = {
+        lastUpdateTS: now
+      }
+      result = true
+    }
+    return result
+  }
+}
+
 
 export default {
   state: {
-    assetsPaginationIds: []
+    assetsPaginationIds: [],
+
+    // {'bitcoin': {lastUpdateTS: timestamp}}
+    assetVolumeChangeCounerUpdages: {}
   },
 
   actions: {
@@ -56,7 +85,7 @@ export default {
     // ),
     // todo: здесь нужна мега-оптимизация
     SOCKET_ON_VOLUME_CHANGE ({ commit }, message)  {
-      if (message.base !== 'bitcoin' && message.quote !== 'bicoin') return
+      // if (message.base !== 'bitcoin' && message.quote !== 'bicoin') return
       // return
       commit('updateVolumeChangeCounter', {coinId: message.base})
       commit('updateVolumeChangeCounter', {coinId: message.quote})
@@ -85,15 +114,20 @@ export default {
         .get()
     }
   },
+
   mutations: {
     addAssetsPaginationIds (state, {newIds}) {
       state.assetsPaginationIds = [...state.assetsPaginationIds, ...newIds]
     },
     updateVolumeChangeCounter (state, {coinId}) {
+      // console.log(this); console.log('^...this:')
+      // console.log('------------')
+      if (!vuexModuleHelpers.canUpdateVolumeChangeCounter(state, coinId)) return
+      // console.log('really processing1')
+      if (!state.assetsPaginationIds.includes(coinId)) return
+      console.log('really processing2')
       Asset.update({
-        where: (asset) => {
-          return [coinId].includes(asset.id)
-        },
+        where: coinId,
         data (asset) {
           asset.tradesCounter += 1
         }
