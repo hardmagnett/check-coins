934abc298cb207b495e6d9cfb6bc4b9535190731
Taras Yemtsov <flamytwista@gmail.com>
2020-01-07 19:02:38 +0300

    one shitty shit is finished

--- src/main.js
+++ src/main.js
@@ -1,12 +1,73 @@
 import 'normalize.css'
 import Vue from 'vue'
+import VueNativeSock from 'vue-native-websocket'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import '@/plugins/index.js'
-import websocketsCoinCap from '@/plugins/vueNativeSock/websocketsCoinCap.js'
+// import websocketsCoinCap from '@/plugins/vueNativeSock/websocketsCoinCap.js'
+
 
 Vue.config.productionTip = false
+
+
+function createPassToStoreHandler(eventsSettings = {}) {
+  return function (currEventName, event) {
+    if (!currEventName.startsWith('SOCKET_')) return
+
+    currEventName = currEventName.replace('SOCKET_', '')
+
+    let eventSettings = {
+      namespace: '',
+      methodType: 'commit',
+      methodName: currEventName.toUpperCase(),
+    }
+
+    const msg = event.data ? JSON.parse(event.data) : {}
+
+    if (eventsSettings[currEventName]) {
+      eventSettings = { ...eventSettings, ...eventsSettings[currEventName] }
+    }
+    eventSettings.methodName = 'SOCKET_' + eventSettings.methodName
+    const methodNameFull = eventSettings.namespace + eventSettings.methodName
+    console.log(methodNameFull); console.log('^...methodNameFull:')
+    this.store[eventSettings.methodType](methodNameFull, msg)
+  }
+}
+
+let passToStoreHandler = createPassToStoreHandler({
+  onmessage: {
+    namespace: 'entities/assets/',
+    methodType: 'dispatch',
+    methodName: 'ON_PRICE_CHANGE',
+  },
+})
+Vue.use(VueNativeSock, 'wss://ws.coincap.io/prices?assets=ALL', {
+  store,
+  format: 'json',
+  reconnection: true,
+  // connectManually: true,
+  passToStoreHandler,
+  passToStoreHandlerZ(eventName, event) {
+    if (!eventName.startsWith('SOCKET_')) { return }
+    const msg = event.data ? JSON.parse(event.data) : {}
+
+    let myVuexMethodType = 'commit' // по умолчанию вызывается мутация
+    let myVuexNamespace = '' // по умолчанию корневой модуль
+    let myVuexMethodName = eventName.toUpperCase()
+
+    if (
+      eventName === 'SOCKET_onmessage'
+    ) {
+      myVuexMethodType = 'dispatch' // вызывает экшн
+      myVuexNamespace = 'entities/assets/'
+      myVuexMethodName = 'SOCKET_ON_PRICE_CHANGE'
+    }
+    const myVuexMethodNameFull = myVuexNamespace + myVuexMethodName
+    // console.log(this)
+    this.store[myVuexMethodType](myVuexMethodNameFull, msg)
+  },
+})
 
 const vm = new Vue({
   router,
@@ -16,6 +77,8 @@ const vm = new Vue({
 
 // todo: вызывать это после получения списка ассетов
 //  Но только после того как напишешь ф-ю реконнекта
-websocketsCoinCap.connectToPrices()
+// websocketsCoinCap.connectToPrices()
 
-export { vm }
+// export { vm }
+
+
--- src/plugins/vueNativeSock/websocketsCoinCap.js
+++ src/plugins/vueNativeSock/websocketsCoinCap.js
@@ -1,7 +1,19 @@
 import Vue from 'vue'
 import VueNativeSock from 'vue-native-websocket'
 import store from '@/store'
-import { vm } from '../../main.js'
+// import { vm } from '../../main.js'
+
+const vm = new Vue()
+
+Vue.use(VueNativeSock, 'wss://ws.coincap.io/', {
+  store,
+  format: 'json',
+  reconnection: true,
+  connectManually: true,
+  // passToStoreHandler(eventName, event) {
+  //   console.log('passToStoreHandler in root')
+  // },
+})
 
 /**
  * Нативные вебсокеты - не особо удобная вещь:
@@ -15,43 +27,71 @@ import { vm } from '../../main.js'
 const websocketsCoinCap = {
   _init() {
     console.log('_init')
-    this._initPrices()
+    // this._initPrices()
   },
   _initPrices() {
     // Ничего страшного что ожидается получение всех (assets=ALL) валют, даже если скроллом загружены ещё не все.
     // Это практически не даёт дополнительной нагрузки. Проверено в Chrome Task Manager.
     // Хотя,бля, есть чё страшное
-    console.log('_initPrices')
-    Vue.use(VueNativeSock, 'wss://ws.coincap.io/prices?assets=ALL', {
-      store,
-      format: 'json',
-      reconnection: true,
-      connectManually: true,
-      passToStoreHandler(eventName, event) {
-        console.log('passToStoreHandler')
-        // console.log('passToStoreHandler')
-        if (!eventName.startsWith('SOCKET_')) { return }
-        const msg = event.data ? JSON.parse(event.data) : {}
-
-        let myVuexMethodType = 'commit' // по умолчанию вызывается мутация
-        let myVuexNamespace = '' // по умолчанию корневой модуль
-        let myVuexMethodName = eventName.toUpperCase()
-
-        if (
-          eventName === 'SOCKET_onmessage'
-        ) {
-          myVuexMethodType = 'dispatch' // вызывает экшн
-          myVuexNamespace = 'entities/assets/'
-          myVuexMethodName = 'SOCKET_ON_PRICE_CHANGE'
-        }
-        const myVuexMethodNameFull = myVuexNamespace + myVuexMethodName
-        this.store[myVuexMethodType](myVuexMethodNameFull, msg)
-      },
-    })
+    // console.log('_initPrices')
+    // Vue.use(VueNativeSock, 'wss://ws.coincap.io/prices?assets=ALL', {
+    //   store,
+    //   format: 'json',
+    //   reconnection: true,
+    //   connectManually: true,
+    //   passToStoreHandler(eventName, event) {
+    //     console.log('passToStoreHandler')
+    //     // console.log('passToStoreHandler')
+    //     if (!eventName.startsWith('SOCKET_')) { return }
+    //     const msg = event.data ? JSON.parse(event.data) : {}
+    //
+    //     let myVuexMethodType = 'commit' // по умолчанию вызывается мутация
+    //     let myVuexNamespace = '' // по умолчанию корневой модуль
+    //     let myVuexMethodName = eventName.toUpperCase()
+    //
+    //     if (
+    //       eventName === 'SOCKET_onmessage'
+    //     ) {
+    //       myVuexMethodType = 'dispatch' // вызывает экшн
+    //       myVuexNamespace = 'entities/assets/'
+    //       myVuexMethodName = 'SOCKET_ON_PRICE_CHANGE'
+    //     }
+    //     const myVuexMethodNameFull = myVuexNamespace + myVuexMethodName
+    //     this.store[myVuexMethodType](myVuexMethodNameFull, msg)
+    //   },
+    // })
   },
   // todo: применить массив id валют, для которых коннектиться
   connectToPrices(assetIds = []) {
-    vm.$connect()
+    console.log('connectToPrices')
+    // return
+    vm.$connect('wss://ws.coincap.io/prices?assets=ALL', {
+      // store,
+      // format: 'json',
+      // reconnection: true,
+      // connectManually: true,
+      // passToStoreHandler(eventName, event) {
+      //   console.log('passToStoreHandler')
+      //   // console.log('passToStoreHandler')
+      //   if (!eventName.startsWith('SOCKET_')) { return }
+      //   const msg = event.data ? JSON.parse(event.data) : {}
+      //
+      //   let myVuexMethodType = 'commit' // по умолчанию вызывается мутация
+      //   let myVuexNamespace = '' // по умолчанию корневой модуль
+      //   let myVuexMethodName = eventName.toUpperCase()
+      //
+      //   if (
+      //     eventName === 'SOCKET_onmessage'
+      //   ) {
+      //     myVuexMethodType = 'dispatch' // вызывает экшн
+      //     myVuexNamespace = 'entities/assets/'
+      //     myVuexMethodName = 'SOCKET_ON_PRICE_CHANGE'
+      //   }
+      //   const myVuexMethodNameFull = myVuexNamespace + myVuexMethodName
+      //   this.store[myVuexMethodType](myVuexMethodNameFull, msg)
+      // },
+    })
+    console.log('connectToPrices 2')
   },
   connectToExchange(exchangeId) {
     // return 10
