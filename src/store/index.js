33b6a5accf661ba2226964245ac33395d909e93c
Taras Yemtsov <flamytwista@gmail.com>
2020-01-09 19:06:28 +0300

    Minor

--- src/store/index.js
+++ src/store/index.js
@@ -9,30 +9,27 @@ export default new Vuex.Store({
   state: {},
   mutations: {
     // мутации-заглушки по умолчанию, чтобы не ругалось в консоль что их нет.
+    // Могут быть полезны для дебага.
     SOCKET_ONOPEN() {
-      console.log('SOCKET_ONOPEN')
+      // console.log('SOCKET_ONOPEN')
     },
     SOCKET_ONCLOSE() {
-      console.log('SOCKET_ONCLOSE')
+      // console.log('SOCKET_ONCLOSE')
     },
     SOCKET_ONERROR() {
-      console.log('SOCKET_ONERROR')
+      // console.log('SOCKET_ONERROR')
     },
     SOCKET_ONMESSAGE() {
-      console.log('SOCKET_ONMESSAGE')
+      // console.log('SOCKET_ONMESSAGE')
     },
     SOCKET_RECONNECT() {
-      console.log('SOCKET_RECONNECT')
+      // console.log('SOCKET_RECONNECT')
     },
     SOCKET_RECONNECT_ERROR() {
-      console.log('SOCKET_RECONNECT_ERROR')
+      // console.log('SOCKET_RECONNECT_ERROR')
     },
   },
-  actions: {
-    // SOCKET_ON_PRICE_CHANGE(state, message) {
-    //   console.log(message); console.log('^...message in action:')
-    // },
-  },
+  actions: {},
   modules: {},
   plugins: [VuexORM.install(database)],
 })
